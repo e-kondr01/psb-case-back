1a9b96d4c22864a9e943f09c40b3d913c08de0ed
Egor Kondrashov <e.kondr01@gmail.com>
2021-12-02 18:18:13 +0300

    feat: move schema urls

--- config/urls.py
+++ config/urls.py
@@ -12,17 +12,18 @@ urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),
 ]
 
+urlpatterns += [
+    path('api/schema/',
+         SpectacularAPIView.as_view(),
+         name='schema'),
+    path('api/schema/swagger-ui/',
+         SpectacularSwaggerView.as_view(url_name='schema'),
+         name='swagger-ui'),
+    path('api/schema/redoc/',
+         SpectacularRedocView.as_view(url_name='schema'),
+         name='redoc'),
+]
+
 if bool(settings.DEBUG):
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
-    urlpatterns += [
-        path('api/schema/',
-             SpectacularAPIView.as_view(),
-             name='schema'),
-        path('api/schema/swagger-ui/',
-             SpectacularSwaggerView.as_view(url_name='schema'),
-             name='swagger-ui'),
-        path('api/schema/redoc/',
-             SpectacularRedocView.as_view(url_name='schema'),
-             name='redoc'),
-    ]
